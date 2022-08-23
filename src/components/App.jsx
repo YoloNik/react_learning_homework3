538668a627c1a112ec0331d9bab632db62001bec
YoloNik <nikitaslipachuk@gmail.com>
2022-08-23 21:00:33 +0300

    test

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Phonebook from './Phonebook/Phonebook';
+import ImageFinder from './ImageFinder/ImageFinder';
+
+export const App = () => {
+  return (
+    <>
+      <Phonebook />
+      <ImageFinder />
+    </>
+  );
+};
---
+++ src/components/common/RegBtn/RegBtn.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './RegBtn.css';
+
+import PropTypes from 'prop-types';
+
+function RegBtn({ handleClick, type, name, children, className }) {
+  return (
+    <button name={name} onClick={handleClick} type={type} className={className}>
+      {children}
+    </button>
+  );
+}
+
+export default RegBtn;
+
+RegBtn.propTypes = {
+  title: PropTypes.string,
+  name: PropTypes.string,
+  handleClick: PropTypes.func,
+};
